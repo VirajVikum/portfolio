df9433978e2fe494cce3004e7b4c8218b62b2058
Viraj Vikum <virajempire2001@gmail.com>
2024-09-17 16:12:14 +0530

    developed the services page

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ import { FiDownload } from "react-icons/fi";
 const Home = () => {
   return (
     // h-full
-    <section className="min-h-screen flex flex-col justify-between">
+    <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
 
